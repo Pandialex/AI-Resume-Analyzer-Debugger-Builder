c1e5dec754bc34457ec9de77a5c0a26202a35bbb
Alex pandian M <146383456+Pandialex@users.noreply.github.com>
2025-10-27 11:54:20 +0530

    Format and clean up accounts URL patterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    # path("send-otp/", views.send_otp_ajax, name="send_otp_ajax"),
+    
     path("forget-password/", views.forget_password_request, name="forget_password_request"),
     # path("forget-password-verify/<int:user_id>/", views.forget_password_verify, name="forget_password_verify"),
     path("profile/", views.profile_view, name="profile"),
@@ -15,4 +15,5 @@ urlpatterns = [
     # path("verify-otp/", views.register, name="verify_otp"),  # fallback: register handles OTP
     path('check-resume-access/', views.check_resume_access, name='check_resume_access'),
 ]
+
 
